11ae092c05c32d5de1a2ecb91c24d6a8d46779cb
Mundo <alanbarasoft@gmail.com>
2020-02-09 18:08:49 +0000

    Memes are retrieved from Azure Storage

--- MemeCommand.ts
+++ MemeCommand.ts
@@ -1,96 +1,166 @@
-import { AbstractCommandModule, Message, OutgoingMessage } from "botyo-api";
+import { AbstractCommandModule, Message, OutgoingMessage, AzureStorage, ContextualizableModuleConfiguration } from "botyo-api";
+import { BlobServiceClient, ContainerClient, BlobClient } from "@azure/storage-blob";
+import { inject } from "inversify";
 import fs = require('fs');
 import path = require('path');
+import * as delay from "delay";
+import * as YAML from "js-yaml";
+import * as intoStream from 'into-stream';
+import { Readable } from "stream";
 
 export default class MemeCommand extends AbstractCommandModule {
-   memes: any;
+    private memes: any;
+    private initialised: boolean = false;
+    private config: ContextualizableModuleConfiguration;
 
-   constructor() {
-      super();
-      const config = this.getRuntime().getConfiguration();
-      this.memes = config.getProperty("memes");
-   }
+    constructor(@inject(AzureStorage.SYMBOL) private readonly azureStorage: BlobServiceClient) {
+        super();
+        this.config = this.getRuntime().getConfiguration();
+        this.initialise();
+        if (this.azureStorage == null) {
+            throw new Error("AzureStorage is null");
+        }
+    }
 
-   get basedir(): string {
-      return process.cwd();
-   }
+    get attachmentsDir(): string {
+        return path.join("attachments");
+    }
 
-   get attachmentsDir(): string {
-      return path.join(this.basedir, "attachments");
-   }
+    async getMemeConfig() {
+        const containerName: string = this.config.getProperty<string>(MemeCommand.CONFIG_AZURE_CONTAINER_NAME);
+        const listFileName: string = this.config.getProperty<string>(MemeCommand.CONFIG_AZURE_LIST_FILE_NAME);
+        const container: ContainerClient = this.azureStorage.getContainerClient(containerName);
+        const memeConfigBlob: BlobClient = container.getBlobClient(listFileName);
+        const memeConfigRawBuffer = await memeConfigBlob.downloadToBuffer();
+        if (!memeConfigRawBuffer.byteLength) {
+            this.getRuntime().getLogger().info("Could not get memes config. Retrying...");
+            await delay(30 * 1000);
+            await this.getMemeConfig();
+            return;
+        }
+        this.memes = YAML.load(memeConfigRawBuffer.toString('utf8')) as object;
+    }
 
-   getCommand(): string {
-      return "meme";
-   }
+    //seems like we can not directly assign stream returned by BlobClient.download() to a message
+    async getFileStream(path: string): Promise<NodeJS.ReadableStream | null> {
+        const containerName: string = this.config.getProperty<string>(MemeCommand.CONFIG_AZURE_CONTAINER_NAME);
+        const container: ContainerClient = this.azureStorage.getContainerClient(containerName);
+        const fileBlob: BlobClient = container.getBlobClient(path);
+        if (!(await fileBlob.exists())) {
+            return null;
+        }
+        let reponse = await fileBlob.download();
+        if (!reponse.readableStreamBody) {
+            return null;
+        }
+        return reponse.readableStreamBody;
+    }
 
-   getDescription(): string {
-      return "Responds to the hello";
-   }
+    async initialise() {
+        this.initialised = false;
 
-   getUsage(): string {
-      return "";
-   }
+        await this.getMemeConfig();
 
-   validate(msg: Message, args: string): boolean {
-      if (!args) {
-         return false;
-      }
+        this.initialised = true;
+    }
 
-      if (!this.checkMemeValid(this.memes, args)) {
-         return false;
-      }
+    getCommand(): string {
+        return "meme";
+    }
 
-      let path: string = this.buildMemePath(args);
+    getDescription(): string {
+        return "Wysyla zaleski mem";
+    }
 
-      if (!fs.existsSync(path)) {
-         return false;
-      }
-      if (fs.lstatSync(path).isDirectory()) {
-         return fs.readdirSync(path).length > 0;
-      }
+    getUsage(): string {
+        return "<nazwa mema lub folder>, wpisz #meme list zeby zobaczyc liste memow";
+    }
 
-      return true;
-   }
+    validate(msg: Message, args: string): boolean {
+        if (!args) {
+            return false;
+        }
 
-   async execute(msg: Message, args: string): Promise<any> {
-      let attachmentPath: string = this.buildMemePath(args);
+        if (args === "list") {
+            return true;
+        }
 
-      if (fs.lstatSync(attachmentPath).isDirectory()) {
-         let files: string[] = fs.readdirSync(attachmentPath);
-         attachmentPath = path.join(attachmentPath, files[Math.floor(Math.random() * files.length)]);
-      }
+        if (!this.checkMemeValid(this.memes, args)) {
+            return false;
+        }
 
-      var message: OutgoingMessage = {
-         attachment: fs.createReadStream(attachmentPath)
-      }
+        return true;
+    }
 
-      return this.getRuntime().getChatApi().sendMessage(msg.threadID, message);
-   }
+    async execute(msg: Message, args: string): Promise<any> {
+        if (!this.initialised) {
+            return this.getRuntime().getChatApi().sendMessage(msg.threadID, "Ladowanie bota w trakcie...");
+        }
+        if (args === "list") {
+            return this.getRuntime().getChatApi().sendMessage(msg.threadID, this.buildMemeList(this.memes, 0));
+        }
+        let attachmentPath: string = this.buildMemePath(args);
 
-   private checkMemeValid(memeDict: any, memePath: string): boolean {
-      const memePathSplitted = memePath.split(" ", 2).filter(Boolean);
-      if (memePathSplitted.length == 2) {
-         if (memeDict[memePathSplitted[0]] != null) {
-            return this.checkMemeValid(memeDict[memePathSplitted[0]], memePathSplitted[1]);
-         }
-      }
-      else if (memePathSplitted.length == 1) {
-         return memePathSplitted[0] in memeDict;
-      }
-      return false;
-   }
+        let attachmmentOldStream: NodeJS.ReadableStream | null = await this.getFileStream(attachmentPath);
+        if (!attachmmentOldStream) {
+            return this.getRuntime().getChatApi().sendMessage(msg.threadID, "Error occured: " + attachmentPath + " not available");
+        }
+        let attachmmentStream = new Readable().wrap(attachmmentOldStream);
 
-   private buildMemePath(memePath: string): string {
-      let result = this.attachmentsDir;
-      let dict = this.memes;
-      const memePathSplitted = memePath.split(" ").filter(Boolean);
-      for (let memePathSeg of memePathSplitted) {
-         if (typeof dict[memePathSeg] === "string") {
-            return path.join(result, dict[memePathSeg]);
-         }
-         dict = dict[memePathSeg];
-         result = path.join(result, memePathSeg);
-      }
-      return result;
-   }
+        //DO NOT REMOVE: hack to provide filetype, the message will fail to upload without this
+        (attachmmentStream as any).path = path.basename(attachmentPath);
+
+        let message: OutgoingMessage = {
+            attachment: attachmmentStream
+        }
+
+        return this.getRuntime().getChatApi().sendMessage(msg.threadID, message);
+    }
+
+    private checkMemeValid(memeDict: any, memePath: string): boolean {
+        const memePathSplitted = memePath.split(" ", 2).filter(Boolean);
+        if (memePathSplitted.length == 2) {
+            if (memeDict[memePathSplitted[0]] != null) {
+                return this.checkMemeValid(memeDict[memePathSplitted[0]], memePathSplitted[1]);
+            }
+        }
+        else if (memePathSplitted.length == 1) {
+            return memePathSplitted[0] in memeDict;
+        }
+        return false;
+    }
+
+    private buildMemePath(memePath: string): string {
+        let result = this.attachmentsDir;
+        let dict = this.memes;
+        const memePathSplitted = memePath.split(" ").filter(Boolean);
+        for (let memePathSeg of memePathSplitted) {
+            if (typeof dict[memePathSeg] === "string") {
+                return path.join(result, dict[memePathSeg]);
+            }
+            dict = dict[memePathSeg];
+            result = path.join(result, memePathSeg);
+        }
+        return result;
+    }
+
+    private buildMemeList(memeDict: any, nest: number): string {
+        let result = "";
+        for (let key in memeDict) {
+            result += "\t".repeat(nest);
+            result += key;
+            if (typeof memeDict[key] === "string") {
+                result += " = " + memeDict[key];
+            }
+            result += "\n";
+            if (memeDict[key] != null && typeof memeDict[key] !== "string") {
+                result += this.buildMemeList(memeDict[key], nest + 1);
+            }
+        }
+        return result;
+    }
+
+
+    static readonly CONFIG_AZURE_CONTAINER_NAME = "azure.meme_container";
+    static readonly CONFIG_AZURE_LIST_FILE_NAME = "azure.meme_list";
 }
